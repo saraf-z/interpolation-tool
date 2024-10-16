01a1422bcd0dda84d914a914d9c94f459f4cecfd
Xandra Campo <xkmpera@gmail.com>
2024-10-16 14:40:37 +0200

    Compute logarithmic energy

--- interpolator.py
+++ interpolator.py
@@ -1,13 +1,18 @@
 # Goal: Interpolate quantity from quantity energy distribution
 # Modeling steps: script, functional programing and OOP programing
+from math import log
 
 # Input data: define some dummy data to test the script
 energy = [1,2,3]  # Energy values of the distribution
 values = [10,20,30]  # Variable values of the distribution
 interpolated_energy = 1.5  # Energy value to interpolate
 
-# Logarithmic transformation
+# Logarithmic transformation: calculate logarithmic values of energy and values
+log_energy = [log(i) for i in energy]
+log_values = None
 
 # Interpolate
 
 # Print results
+print(f'Energy: {energy}')
+print(f'Logarithmic energy: {log_energy}')
