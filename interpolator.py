300f38534ccc935570d1b6e52ee9600992cc019d
Xandra Campo <xkmpera@gmail.com>
2024-10-16 14:27:33 +0200

    Add dummy input data for interpolation

--- interpolator.py
+++ interpolator.py
@@ -1,7 +1,10 @@
 # Goal: Interpolate quantity from quantity energy distribution
 # Modeling steps: script, functional programing and OOP programing
 
-# Input data
+# Input data: define some dummy data to test the script
+energy = [1,2,3]  # Energy values of the distribution
+values = [10,20,30]  # Variable values of the distribution
+interpolated_energy = 1.5  # Energy value to interpolate
 
 # Logarithmic transformation
 
