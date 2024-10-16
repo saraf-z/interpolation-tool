a4d130d799846c854dcea1684660e1898d22060b
Xandra Campo <xkmpera@gmail.com>
2024-10-16 14:25:08 +0200

    Add interpolator script steps

---
+++ interpolator.py
@@ -0,0 +1,10 @@
+# Goal: Interpolate quantity from quantity energy distribution
+# Modeling steps: script, functional programing and OOP programing
+
+# Input data
+
+# Logarithmic transformation
+
+# Interpolate
+
+# Print results
