add5a71c5ccd13eea709ec0b6b69c538e20d76af
saraf-z <sfdezamb@gmail.com>
2024-11-04 12:30:49 +0100

    Clean code

--- interpolation_functions.py
+++ interpolation_functions.py
@@ -3,7 +3,7 @@ from idlelib.autocomplete import FILES
 
 import numpy as np
 import pandas as pd
-from scipy.interpolate import Akima1DInterpolator
+from scipy.interpolate import Akima1DInterpolator, interp1d
 from math import log, exp
 from openpyxl import load_workbook
 
@@ -167,44 +167,6 @@ def main():
     # Step 7: Convert to exp
     interpolated_value = np.exp(interpolated_log_value)
     print(f"Interpolated value for energy {interpolated_energy} keV is approximately {interpolated_value}")
-
-
-
-
-
-
- #def cli():
-    # Command Line Interface to get input from the user
-    # # Step 1: Get input for energy and values from the user
-    # # print('Introduce data for energy (comma-separated, e.g., "1, 2, 3"), then press enter:')
-    # # energy_input = input()
-    # #
-    # # print('Introduce data for values (comma-separated, e.g., "10, 20, 30"), then press enter:')
-    # # values_input = input()
-    #
-    # # Step 2: Get input for the interpolated energy value
-    print('Introduce interpolated energy (a single number from 20.0 to 400, e.g., "22.3"):')
-    interpolated_energy = float(input())  # Convert the user input to a float
-    # # Step 3: Perform the logarithmic transformation
-    # log_energy, log_values, log_interpolated_energy = take_logarithm(energy, values, interpolated_energy)
-    #
-    # # Step 4: Perform the interpolations
-    # log_interpolated_value = log_linear_interpolation(log_energy, log_values, log_interpolated_energy)
-    # log_interpolated_value_akima = log_akima_interpolation(log_energy, log_values, log_interpolated_energy)
-    #
-    # # Step 5: Convert log-interpolated values to normal values
-    # interpolated_value, interpolated_value_akima = log_to_value(log_interpolated_value, log_interpolated_value_akima)
-    #
-    # # Step 6: Print the results
-    # print(f'Log Interpolated Value (Linear): {log_interpolated_value}')
-    # print(f'Log Interpolated Value (Akima): {log_interpolated_value_akima}')
-    # print(f'Interpolated Value (Linear): {interpolated_value}')
-    # print(f'Interpolated Value (Akima): {interpolated_value_akima}')
-    #
-    #
-
-
-
 
 
 # main block
